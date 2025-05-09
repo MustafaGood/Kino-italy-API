fa85dd2f4c63d20a336864a95bb9e56a66e22b86
Revoxxi <coff@live.se>
2025-05-09 10:04:43 +0200

    La till kommentar för att påvisa temporära ändringar i server.js

--- kino-webbplats-main/backend/server.js
+++ kino-webbplats-main/backend/server.js
@@ -11,7 +11,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, '../frontend')));
 app.use(express.static(path.join(__dirname, '../public')));
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(express.json()); //REMOVE/TA BORT NÄR VI INTE BEHÖVER JSON och använder MongoDB istället
 
 app.get('/', async (req, res) => {
   try {
